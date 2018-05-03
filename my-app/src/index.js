f1d1928c61410923d5708feff77bdf81933dab83
lty <380659407@qq.com>
2018-05-03 15:44:25 +0800

    demo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ClickCounter from "./ClickCounter.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <ClickCounter />,
+    document.getElementById('root')
+);
